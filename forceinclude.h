0c9f71f271ca5671857a2e9d175bb16b3820eeb8
grant <abernathy.grant@icloud.com>
2025-07-19 14:14:30 -0500

    Anotha one

--- forceinclude.h
+++ forceinclude.h
@@ -2,6 +2,6 @@
 #define INC_FORCEINCLUDE_H_
 
 // Disable warnings for now
-#pragma warning(disable : 4996 4244 4091 4305 4474 4477 4603 4101 4172 4267 4309)
+#pragma warning(disable : 4996 4244 4091 4305 4474 4477 4603 4101 4172 4267 4309 4018)
 
 #endif // INC_FORCEINCLUDE_H_
