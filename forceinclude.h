d4ccbf37e364885238b189c099bb8a3814ce1eb4
grant <abernathy.grant@icloud.com>
2025-07-19 14:11:58 -0500

    Ignore more warnings

--- forceinclude.h
+++ forceinclude.h
@@ -2,6 +2,6 @@
 #define INC_FORCEINCLUDE_H_
 
 // Disable warnings for now
-#pragma warning(disable : 4996 4244 4091 4305 4474 4477 4603)
+#pragma warning(disable : 4996 4244 4091 4305 4474 4477 4603 4101 4172 4267 4309)
 
 #endif // INC_FORCEINCLUDE_H_
