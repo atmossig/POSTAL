6db493e36b0e9086970007697dd771a12bf52bcc
grant <abernathy.grant@icloud.com>
2025-07-19 14:11:11 -0500

    Ignore warnings vs2017 toolset

---
+++ forceinclude.h
@@ -0,0 +1,7 @@
+#ifndef INC_FORCEINCLUDE_H_
+#define INC_FORCEINCLUDE_H_
+
+// Disable warnings for now
+#pragma warning(disable : 4996 4244 4091 4305 4474 4477 4603)
+
+#endif // INC_FORCEINCLUDE_H_
